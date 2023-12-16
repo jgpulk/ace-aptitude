a6ae9c8ab1e34dc29ac3df0726359ba1b145a4f7
Joyal George <joyalgeorgepulk@gmail.com>
2023-12-17 00:59:42 +0530

    added basic registration validation - server side

--- app/Controllers/User.php
+++ app/Controllers/User.php
@@ -14,4 +14,23 @@ class User extends BaseController
     public function register(){
         return view('register');
     }
+
+    public function register_submission(){
+        $rules = [
+            'name' => ['rules' => 'required|min_length[3]|max_length[255]'],
+            'email' => ['rules' => 'required|valid_email|is_unique[user.email]'],
+            'phone' => ['rules' => 'required|min_length[10]|max_length[20]'],
+            'password' => ['rules' => 'required|min_length[6]'],
+            're_password' => ['rules' => 'required|matches[password]'],
+        ];
+        
+        if($this->validate($rules)){
+            echo "Validation success";
+        } else{
+            echo "Validaton dailed";
+            echo "<pre>";
+            print_r($this->validator->getErrors());
+            echo "</pre>";
+        }
+    }
 }
