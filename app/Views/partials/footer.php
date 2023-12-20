2bb391199cfd17dd32a724c592e80c088b235164
Joyal George <joyalgeorgepulk@gmail.com>
2023-12-20 21:56:46 +0530

    Added common navbar and footer for user

---
+++ app/Views/partials/footer.php
@@ -0,0 +1,16 @@
+<footer class="footer-admin mt-auto footer-light">
+    <div class="container-xl px-4">
+        <div class="row">
+            <div class="col-md-6 small">Copyright © Ace Aptitude <span id="year"></span></div>
+            <div class="col-md-6 text-md-end small">
+                <a href="#">Privacy Policy</a>
+                ·
+                <a href="#">Terms &amp; Conditions</a>
+            </div>
+        </div>
+    </div>
+</footer>
+
+<script>
+    document.getElementById("year").innerHTML = new Date().getFullYear();
+</script>
