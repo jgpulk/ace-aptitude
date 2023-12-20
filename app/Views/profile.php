5050cf2d2860df976db854b1247af28a9bea422d
Joyal George <joyalgeorgepulk@gmail.com>
2023-12-21 00:12:15 +0530

    added security tab design

--- app/Views/profile.php
+++ app/Views/profile.php
@@ -96,7 +96,39 @@
                             </div>
                         </div>
                         <div class="tab-pane fade" id="security" role="tabpanel" aria-labelledby="security-tab">
-                            <h5 class="card-title">Security Settings</h5>
+                            <div class="row">
+                                <div class="col-lg-8">
+                                    <div class="card mb-4">
+                                        <div class="card-header">Change Password</div>
+                                        <div class="card-body">
+                                            <form>
+                                                <div class="mb-3">
+                                                    <label class="small mb-1" for="currentPassword">Current Password</label>
+                                                    <input class="form-control" id="currentPassword" type="password" placeholder="Enter current password" />
+                                                </div>
+                                                <div class="mb-3">
+                                                    <label class="small mb-1" for="newPassword">New Password</label>
+                                                    <input class="form-control" id="newPassword" type="password" placeholder="Enter new password" />
+                                                </div>
+                                                <div class="mb-3">
+                                                    <label class="small mb-1" for="confirmPassword">Confirm Password</label>
+                                                    <input class="form-control" id="confirmPassword" type="password" placeholder="Confirm new password" />
+                                                </div>
+                                                <button class="btn btn-primary" type="button">Save</button>
+                                            </form>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-lg-4">
+                                    <div class="card mb-4">
+                                        <div class="card-header">Delete Account</div>
+                                        <div class="card-body">
+                                            <p>Deleting your account is a permanent action and cannot be undone. If you are sure you want to delete your account, select the button below.</p>
+                                            <button class="btn btn-danger-soft text-danger disabled" type="button">I understand, delete my account</button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
                         <div class="tab-pane fade" id="notification" role="tabpanel" aria-labelledby="notification-tab">
                             <h5 class="card-title">Notifications Settings</h5>
