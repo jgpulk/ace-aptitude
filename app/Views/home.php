3aef2111dba02f38ad50f92ccf04bfa79ed34a57
Joyal George <joyalgeorgepulk@gmail.com>
2023-12-31 21:56:28 +0530

    changed navbar for not authenticated users

--- app/Views/home.php
+++ app/Views/home.php
@@ -4,8 +4,15 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Home | Ace Aptitude</title>
+    <link rel="stylesheet" href="<?php echo base_url('css/sb-admin-pro/styles.css'); ?>">
+    <script data-search-pseudo-elements="" defer="" src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/js/all.min.js" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/feather-icons/4.29.0/feather.min.js" crossorigin="anonymous"></script>
 </head>
-<body>
-    <h1>Home</h1>
+<body class="nav-fixed">
+    <?php require('partials/navbar.php'); ?>
+    
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
+    <script src="<?php echo base_url('js/sb-admin-pro/scripts.js') ?>"></script>
+    <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
 </body>
 </html>
--- app/Views/partials/navbar.php
+++ app/Views/partials/navbar.php
@@ -32,25 +32,37 @@
 
         <!-- User Dropdown-->
         <li class="nav-item dropdown no-caret dropdown-user me-3 me-lg-4">
-            <a class="btn btn-icon btn-transparent-dark dropdown-toggle" id="navbarDropdownUserImage" href="javascript:void(0);" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><img class="img-fluid" src="https://sb-admin-pro.startbootstrap.com/assets/img/illustrations/profiles/profile-1.png" /></a>
-            <div class="dropdown-menu dropdown-menu-end border-0 shadow animated--fade-in-up" aria-labelledby="navbarDropdownUserImage">
-                <h6 class="dropdown-header d-flex align-items-center">
-                    <img class="dropdown-user-img" src="https://sb-admin-pro.startbootstrap.com/assets/img/illustrations/profiles/profile-1.png" />
-                    <div class="dropdown-user-details">
-                        <div class="dropdown-user-details-name"><?php echo  $navbar_data['name'] ?></div>
-                        <div class="dropdown-user-details-email"><?php echo  $navbar_data['email'] ?></div>
-                    </div>
-                </h6>
-                <div class="dropdown-divider"></div>
-                <a class="dropdown-item" href="<?php echo base_url('user/account') ?>">
-                    <div class="dropdown-item-icon"><i data-feather="settings"></i></div>
-                    Account
-                </a>
-                <a class="dropdown-item" href="<?php echo base_url('user/logout') ?>">
-                    <div class="dropdown-item-icon"><i data-feather="log-out"></i></div>
-                    Logout
-                </a>
-            </div>
+            <?php 
+                if(!(session()->has('is_user_logged_in'))){
+                    ?>
+                        <a href="<?php echo site_url('user/login')?>">
+                            <button class="btn btn-outline-primary" type="button">Login</button>
+                        </a>
+                    <?php
+                } else{
+                    ?>
+                        <a class="btn btn-icon btn-transparent-dark dropdown-toggle" id="navbarDropdownUserImage" href="javascript:void(0);" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><img class="img-fluid" src="https://sb-admin-pro.startbootstrap.com/assets/img/illustrations/profiles/profile-1.png" /></a>
+                        <div class="dropdown-menu dropdown-menu-end border-0 shadow animated--fade-in-up" aria-labelledby="navbarDropdownUserImage">
+                            <h6 class="dropdown-header d-flex align-items-center">
+                                <img class="dropdown-user-img" src="https://sb-admin-pro.startbootstrap.com/assets/img/illustrations/profiles/profile-1.png" />
+                                <div class="dropdown-user-details">
+                                    <div class="dropdown-user-details-name"><?php echo  $navbar_data['name'] ?></div>
+                                    <div class="dropdown-user-details-email"><?php echo  $navbar_data['email'] ?></div>
+                                </div>
+                            </h6>
+                            <div class="dropdown-divider"></div>
+                            <a class="dropdown-item" href="<?php echo base_url('user/account') ?>">
+                                <div class="dropdown-item-icon"><i data-feather="settings"></i></div>
+                                Account
+                            </a>
+                            <a class="dropdown-item" href="<?php echo base_url('user/logout') ?>">
+                                <div class="dropdown-item-icon"><i data-feather="log-out"></i></div>
+                                Logout
+                            </a>
+                        </div>
+                    <?php
+                }
+            ?>
         </li>
     </ul>
 </nav>
