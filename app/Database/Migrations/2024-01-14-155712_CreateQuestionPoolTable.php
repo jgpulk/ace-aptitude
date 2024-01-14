5e4651cb56f327cda80793be014719a0485e6f3e
Joyal George <joyalgeorgepulk@gmail.com>
2024-01-15 00:06:27 +0530

    added question pool migration

---
+++ app/Database/Migrations/2024-01-14-155712_CreateQuestionPoolTable.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class CreateQuestionPoolTable extends Migration
+{
+    public function up()
+    {
+        $this->forge->addField([
+            'question_id' => [
+                'type' => 'INT',
+                'unsigned' => true,
+                'auto_increment' => true,
+                'primary' => true,
+            ],
+            'section' => [
+                'type' => 'VARCHAR',
+                'constraint' => 255
+            ],
+            'sub_section' => [
+                'type' => 'VARCHAR',
+                'constraint' => 255,
+                'default' => NULL
+            ],
+            'question' => [
+                'type' => 'TEXT'
+            ],
+            'option_a' => [
+                'type' => 'TEXT'
+            ],
+            'option_b' => [
+                'type' => 'TEXT'
+            ],
+            'option_c' => [
+                'type' => 'TEXT',
+                'default' => NULL
+            ],
+            'option_d' => [
+                'type' => 'TEXT',
+                'default' => NULL
+            ],
+            'option_e' => [
+                'type' => 'TEXT',
+                'default' => NULL
+            ],
+            'option_f' => [
+                'type' => 'TEXT',
+                'default' => NULL
+            ],
+            'answer' => [
+                'type' => 'TINYINT'
+            ],
+            'explanation' => [
+                'type' => 'TEXT'
+            ],
+            'difficulty' => [
+                'type' => 'TEXT'
+            ],
+            'status' => [
+                'type' => 'TINYINT',
+                'default' => 1
+            ]
+        ]);
+
+        $this->forge->addKey('question_id', true);
+        $this->forge->createTable('question_pool');
+    }
+
+    public function down()
+    {
+        $this->forge->dropTable('question_pool');
+    }
+}
