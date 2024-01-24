f11c60fbf8c33bde7ac6b71e3c9f056947833324
Joyal George <joyalgeorgepulk@gmail.com>
2024-01-24 22:12:00 +0530

    updated side nav bar for generating test option

--- app/Views/admin/partials/sidenavbar.php
+++ app/Views/admin/partials/sidenavbar.php
@@ -22,6 +22,16 @@
                         <a class="nav-link" href="javascript:void(0);">List</a>
                     </nav>
                 </div>
+                <a class="nav-link collapsed <?php echo ($active_tab=='assessment')?'active':''?>" id="assessment" href="javascript:void(0);" data-bs-toggle="collapse" data-bs-target="#collapseAssessment" aria-expanded="false" aria-controls="collapseAssessment">
+                    <div class="nav-link-icon"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-activity"><polyline points="22 12 18 12 15 21 9 3 6 12 2 12"></polyline></svg></div>
+                    Assessment
+                    <div class="sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
+                </a>
+                <div class="collapse" id="collapseAssessment" data-bs-parent="#accordionSidenav">
+                    <nav class="sidenav-menu-nested nav accordion" id="accordionSidenavPages">
+                        <a class="nav-link" href="javascript:void(0);">Generate Test</a>
+                    </nav>
+                </div>
             </div>
         </div>
     </nav>
